ebbd47643236dfc50d27f35734ab6b4ab85c5025
Sean A <37087075+sean-gits-py@users.noreply.github.com>
2021-04-02 14:34:48 -0500

    Update and rename import_ETA539 to import_ETA539.py

--- import_ETA539.py
+++ import_ETA539.py
@@ -9,3 +9,5 @@ df = pd.read_csv(csv_url)
 df.columns = ['state', 'report_date', 'wk_nbr', 'claim_date', 'initial_claims', 'fed_init_claims', 'fmr_servicemembers_init_claims', 'wrkshare_init_claims', 'wrkshare_equiv_init_claims', 'contd_wks_claims', 'fed_contd_wks_claims', 'fmr_serv_memb_contd_wks_claims', 'wrkshare_contd_wks_claims', 'wrkshare_equiv_contd_wks_claims', 'extd_bnft_prog_contd_wks_claims', 'extd_bnft_addl_bnft_prog_contd_wks_claims', 'state_addl_bnft_prog_contd_wks_claims_a', 'state_addl_bnft_prog_contd_wks_claims_b', 'avg_adj_total_contd_wks_claims', 'qtr_avg_monthly_covered_employment', 'rate_of_insured unemployment', 'Avg_rate_of_insured_unemployment_last_two_yrs', 'curr_rate_prct_avg_rate_last_two_yrs', 'state_extd_bnft_period_beg_end', 'status_chg_date', 'curdate', 'prior_wk_pub', 'prior_wk']
 
 df.head()
+
+#
